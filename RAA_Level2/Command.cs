fc34992e9a25b926d4f4d0a3463488251cc914ba
Marco Barboza <Marco.Barboza@smithgroup.com>
2023-02-06 02:35:08 -0800

    Add project files.

---
+++ RAA_Level2/Command.cs
@@ -0,0 +1,163 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+
+#endregion
+
+namespace RAA_Level2
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // Instantiate window
+            ProjectSetup win = new ProjectSetup()
+            {
+                Width = 600,
+                Height = 450,
+                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
+                Topmost = true,
+            };
+            
+            win.ShowDialog();
+
+            if (win.DialogResult == false)
+            {
+                return Result.Cancelled;
+            }
+
+            // Begin Revit transaction
+            List<Level> newLevels = new List<Level>();
+            List<ViewPlan> newFloorPlans = new List<ViewPlan>();
+            List<ViewPlan> newRCPs = new List<ViewPlan>();
+
+            Transaction trans = new Transaction(doc, "Create Levels + Views");
+            try
+            {
+                trans.Start();
+
+                // Get existing document Levels and elevations
+                var existingLevels = new FilteredElementCollector(doc)
+                                        .OfClass(typeof(Level))
+                                        .ToElements()
+                                        .Cast<Level>();
+
+                List<double> existingElevations = existingLevels.Select(x => x.Elevation).ToList();
+
+                foreach (string[] arr in win.FileData)
+                {
+                    string levelName = arr[0];
+                    string elevation = win.Units == ProjectUnits.Imperial ? arr[1] : arr[2];
+
+                    bool isNumber = double.TryParse(elevation, out double levelElevation);
+
+                    if (isNumber)
+                    {
+                        // Check Level with same elevation does not exist
+                        if (!existingElevations.Contains(levelElevation))
+                        {
+                            Level level = Level.Create(doc, levelElevation);
+                            level.Name = levelName;
+                            newLevels.Add(level);
+
+                            if (win.CreateFloorPlans)
+                            {
+                                ViewFamilyType planFamilyType = GetViewFamilyTypeByName(doc, "Floor Plan", ViewFamily.FloorPlan);
+                                ViewPlan plan = ViewPlan.Create(doc, planFamilyType.Id, level.Id);
+                                newFloorPlans.Add(plan);
+                            }
+
+                            if (win.CreateRCPs)
+                            {
+                                ViewFamilyType rcpFamilyType = GetViewFamilyTypeByName(doc, "Ceiling Plan", ViewFamily.CeilingPlan);
+                                ViewPlan rcp = ViewPlan.Create(doc, rcpFamilyType.Id, level.Id);
+                                newRCPs.Add(rcp);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        TaskDialog.Show("Error", $"The item in the Elevation column is not a number.\n{levelName} will be skipped.");
+                        continue;
+                    }
+                }
+
+                trans.Commit();
+
+                if (trans.GetStatus() == TransactionStatus.Committed)
+                {
+                    if (newLevels.Count > 0)
+                    {
+                        string msg = $"New Levels created: {newLevels.Count}";
+
+                        if (newFloorPlans.Count > 0)
+                        {
+                            string plansMsg = $"\n\nNew Floor Plan views created: {newFloorPlans.Count}";
+                            msg += plansMsg;
+                        }
+
+                        if (newRCPs.Count > 0)
+                        {
+                            string rcpsMsg = $"\n\nNew Reflected Ceiling Plan views created: {newRCPs.Count}";
+                            msg += rcpsMsg;
+                        }
+
+                        TaskDialog.Show("Success - Created Levels + Views", msg);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Failed to Create Levels / Views", ex.Message.ToString());
+                trans.RollBack();
+            }
+            finally
+            {
+                trans.Dispose();
+            }
+
+            return Result.Succeeded;
+        }
+
+        private ViewFamilyType GetViewFamilyTypeByName(Document doc, string typeName, ViewFamily viewFamily)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(ViewFamilyType));
+
+            foreach (ViewFamilyType vft in collector)
+            {
+                if (((vft.Name == typeName) || (vft.FamilyName == typeName)) && (vft.ViewFamily == viewFamily))
+                {
+                    return vft;
+                }
+            }
+
+            return null;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
+
---
+++ RAA_Level2/Forms/ProjectSetup.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace RAA_Level2
+{
+    /// <summary>
+    /// Interaction logic for Window.xaml
+    /// </summary>
+    public partial class ProjectSetup : Window
+    {
+        private readonly string _defaultFile;
+        private string _selectedFile = "";
+        private List<string[]> _fileData;
+        private ProjectUnits _projectUnits;
+        //private readonly List<KeyValuePair<string, bool>> _viewsToCreate;
+
+        public ProjectSetup()
+        {
+            InitializeComponent();
+            _defaultFile = "C:\\Downloads\\ProjectSetup.csv";
+            txbFilePath.Text = _defaultFile;
+        }
+
+        public string SelectedFile 
+        {
+            get { return _selectedFile; }
+        }
+
+        public List<string[]> FileData
+        {
+            get { return _fileData; }
+        }
+
+        public ProjectUnits Units
+        {
+            get { return _projectUnits; }
+            private set { _projectUnits = value; }
+        }
+
+        public bool CreateFloorPlans 
+        {
+            get { return cbxCreateFloorPlans.IsChecked == true; } 
+        }
+        public bool CreateRCPs 
+        {
+            get { return cbxCreateRCPs.IsChecked == true; } 
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Recent),
+                Filter = "csv files (*.csv)|*.csv"
+            };
+
+            if (openFile.ShowDialog(this) == true)
+            {
+                _selectedFile = openFile.FileName;
+                txbFilePath.Text = _selectedFile;
+                btnOK.IsEnabled = true;
+            }
+            else
+            {
+                // Do nothing
+            }
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            if (_selectedFile == "")
+            {
+                MessageBox.Show("Please select a file (*.csv) to create Levels / Plan views from.", "Warning - No file selected");
+                return;
+            }
+
+            _fileData = ReadCsvFile(_selectedFile, true);
+            _projectUnits = GetProjectUnits();
+
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Eventually move to separate ViewModel or Util class for MVVM.
+        private List<string[]> ReadCsvFile(string file, bool removeHeader = false)
+        {
+            List<string[]> fileData = new List<string[]>();
+            string[] dataRows = File.ReadAllLines(file);
+
+            foreach (string row in dataRows)
+            {
+                string[] rowCells = row.Split(',');
+                fileData.Add(rowCells);
+            }
+
+            if (removeHeader)
+            {
+                fileData.RemoveAt(0);
+            }
+
+            return fileData;
+        }
+
+        private ProjectUnits GetProjectUnits()
+        {
+            return rbnImperial.IsChecked == true 
+                ? ProjectUnits.Imperial 
+                : ProjectUnits.Metric;
+        }
+    }
+
+    public enum ProjectUnits
+    {
+        Metric,
+        Imperial
+    }
+}
