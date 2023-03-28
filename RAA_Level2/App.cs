1c0cbbf3e68dbeaee1bdbefd0bb7076e217b20ee
Marco Barboza <Marco.Barboza@smithgroup.com>
2023-03-27 18:21:10 -0700

    Add pushbutton to ribbonpanel

--- RAA_Level2/App.cs
+++ RAA_Level2/App.cs
@@ -33,10 +33,12 @@ namespace RAA_Level2
             // 3. Create button data instances
             ButtonDataClass projectSetupButtonData = new ButtonDataClass("ProjectSetup", "Project\rSetup", ProjectSetupCommand.GetMethod(), Properties.Resources.Blue_32, Properties.Resources.Blue_16, "Setup initial Levels and Views from a CSV file.");
             ButtonDataClass viewRenumberButtonData = new ButtonDataClass("ViewRenumber", "View\rRenumber", ViewRenumberCommand.GetMethod(), Properties.Resources.Yellow_32, Properties.Resources.Yellow_16, "Renumber views in a sheet.");
+            ButtonDataClass sheetMakerButtonData = new ButtonDataClass("SheetMaker", "Sheet\rMaker", SheetMakerCommand.GetMethod(), Properties.Resources.Red_32, Properties.Resources.Red_16, "Create new Sheets within the project.");
 
             // 4. Create buttons
             PushButton projectSetupButton = panel.AddItem(projectSetupButtonData.Data) as PushButton;
             PushButton viewRenumberButton = panel.AddItem(viewRenumberButtonData.Data) as PushButton;
+            PushButton sheetMakerButton = panel.AddItem(sheetMakerButtonData.Data) as PushButton;
 
             return Result.Succeeded;
         }
